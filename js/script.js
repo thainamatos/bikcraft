234a3f8e35c5ec7e937124c9fc2f640e694e834e
Thaina Matos <thainamatos.if@hotmail.com>
2022-08-18 17:30:38 -0300

    adicionando plugin e atualizacoes

--- js/script.js
+++ js/script.js
@@ -44,3 +44,27 @@ function eventosPerguntas(pergunta){
     pergunta.addEventListener('click', ativarPergunta)
 }
 perguntas.forEach(eventosPerguntas);
+
+//GALERIA DE BICICLETAS
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+    const img = event.currentTarget;
+    const media = matchMedia("(min-width: 1000px)").matches;
+    if (media) {
+      galeriaContainer.prepend(img);
+    }
+  }
+
+function eventosGaleria(img) {
+    img.addEventListener("click", trocarImagem);
+  }
+
+galeria.forEach(eventosGaleria);
+
+//Animação
+if(window.SimpleAnime){
+new SimpleAnime();
+}
